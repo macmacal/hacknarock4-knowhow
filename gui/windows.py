e72102666ee2e2f74b950fb33d6d960023226857
macmacal <mac.aleksandrowiczgmail.com>
2020-08-29 16:44:05 +0200

    Setting text vals in PassiveDataInspector

--- gui/windows.py
+++ gui/windows.py
@@ -1,11 +1,17 @@
+from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
-from kivy.uix.boxlayout import BoxLayout
-
-
-class WindowManager(ScreenManager):
-    pass
+ID = "123456789"
+NAME = "Drukarka"
+PRODUCER = "BIGBRO"
+MODEL = "DCP-J315"
+SERIAL_NUMBER = "123456"
+ACTIVATION_DATE = "DEJT 1"
+PURCHASE_DATE = "DEJT 2"
+PORTS = "PORTS 1, PORTS 2"
+OTHER = "AHJOs"
 
 
 class MainWindow(Screen):
@@ -13,8 +19,39 @@ class MainWindow(Screen):
 
 
 class PassiveDataInspector(Screen):
+    item_id = ObjectProperty(None)
+    namee = ObjectProperty(None)
+    producer = ObjectProperty(None)
+    model = ObjectProperty(None)
+    serial_number = ObjectProperty(None)
+    activation_date = ObjectProperty(None)
+    purchase_date = ObjectProperty(None)
+    ports = ObjectProperty(None)
+    other = ObjectProperty(None)
+
+    def on_enter(self, *args):
+        self.item_id.text = ID
+        self.namee.text = NAME
+        self.producer.text = PRODUCER
+        self.model.text = MODEL
+        self.serial_number.text = SERIAL_NUMBER
+        self.activation_date.text = ACTIVATION_DATE
+        self.purchase_date.text = PURCHASE_DATE
+        self.ports.text = PORTS
+        self.other.text = OTHER
+
+class WindowManager(ScreenManager):
     pass
 
 
 kivy_builder = Builder.load_file("./gui/windows.kv")
+sm = WindowManager()
 
+screens = [PassiveDataInspector(name="passive_data_inspector"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+
+class AppMain(App):
+    def build(self):
+        return kivy_builder
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from kivy.app import App
 import gui.windows
 
-
-class MyApp(App):
-    def build(self):
-        return gui.windows.kivy_builder
-
-
 if __name__ == "__main__":
-    MyApp().run()
+    gui.windows.AppMain().run()
