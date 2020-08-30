f7e200bc55b7afc4a432555c2dbbe9ad5b5e3aef
Tomasz <kurek.tom@gmail.com>
2020-08-30 02:57:23 +0200

    Add document DAO

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -5,5 +5,6 @@ import constants
 
 def copy_pdf(path_from, asset_id):
     split_path = path_from.split('\\')
-    print(split_path[len(split_path)-1])
-    shutil.copyfile(path_from, constants.DOCUMENTS_PATH + '\\' + str(asset_id) + '_' + split_path[len(split_path)-1])
+    new_path = constants.DOCUMENTS_PATH + '\\' + str(asset_id) + '_' + split_path[len(split_path)-1]
+    shutil.copyfile(path_from, new_path)
+    return new_path
