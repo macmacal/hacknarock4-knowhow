2085f81e8e8b7f5a66ea342d68fa31752a125c16
Tomasz <kurek.tom@gmail.com>
2020-08-30 03:31:18 +0200

    Add function to remove all pdf

--- database/DocumentDAO.py
+++ database/DocumentDAO.py
@@ -9,6 +9,11 @@ class DocumentDAO:
     def __init__(self, documents_path):
         self.documents_path = documents_path
 
+    def remove_all_documents_from_id(self, id):
+        paths = glob.glob(self.documents_path + '\\' + str(id) + '*')
+        for path in paths:
+            remove(path)
+
     def remove_document(self, id, name):
         paths = glob.glob(self.documents_path + '\\' + str(id) + '_' + name)
         if len(paths) == 1:
