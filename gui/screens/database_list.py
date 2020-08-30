eebeb12b845595f2621fdc0af500c04530492f9c
macmacal <mac.aleksandrowiczgmail.com>
2020-08-30 04:11:46 +0200

    camera + codebars

--- gui/screens/database_list.py
+++ gui/screens/database_list.py
@@ -3,6 +3,9 @@ from kivy.properties import ObjectProperty
 from database.EntityDAO import DataDAO
 from gui.popup import information_poup
 from data.PassiveData import PassiveData
+from PIL import Image
+from pyzbar.pyzbar import decode
+from VideoCapture import Device
 
 
 class DatabaseList(Screen):
@@ -15,6 +18,19 @@ class DatabaseList(Screen):
         for it in DataDAO.get_all_data():
             data_list.append('{}  -  {}'.format(str(it.id), str(it.name)))
         return "\n".join(data_list)
+
+    def btn_read_barcode(self):
+        try:
+            cam = Device()
+            cam.saveSnapshot('codebars/photo.png')
+        except:
+            information_poup(msg='Failed to capture a photo!')
+            return
+        try:
+            data_decoded = decode(Image.open('codebars/photo.png'))
+            self.id_input.text = data_decoded[0][0]
+        except:
+            information_poup(msg='Failed to read barcode!')
 
     def btn_inspect(self):
         if self.id_input.text == '' and self.name_input.text == '':
